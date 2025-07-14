9f44664c83813aaaae81e35f4b97662967f30991
vikkibaby1320 <vikkibaby1320@gmail.com>
2025-07-14 16:26:22 +0530

    fix: add missing Login.js page

---
+++ src/pages/Login.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Login = () => {
+  return (
+    <div style={{color: "#fff", background: "#222", minHeight: "100vh", display: "flex", alignItems: "center", justifyContent: "center"}}>
+      <h2>Login Page Coming Soon</h2>
+    </div>
+  );
+};
+
+export default Login;

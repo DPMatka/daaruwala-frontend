874449223975a16aa8103d40111373b7b6468026
vikkibaby1320 <vikkibaby1320@gmail.com>
2025-07-14 16:28:50 +0530

    fix: add missing ShoppingCart.js page

---
+++ src/pages/ShoppingCart.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ShoppingCart = () => {
+  return (
+    <div style={{color: "#fff", background: "#222", minHeight: "100vh", display: "flex", alignItems: "center", justifyContent: "center"}}>
+      <h2>Shopping Cart Page Coming Soon</h2>
+    </div>
+  );
+};
+
+export default ShoppingCart;

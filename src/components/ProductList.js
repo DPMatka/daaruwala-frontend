15660a64f231edb236778016cc689fd3e26d96ef
vikkibaby1320 <vikkibaby1320@gmail.com>
2025-07-14 16:37:14 +0530

    fix: add missing ProductList.js component

---
+++ src/components/ProductList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ProductList = () => {
+  return (
+    <div style={{color: "#fff", background: "#222", minHeight: "100vh", display: "flex", alignItems: "center", justifyContent: "center"}}>
+      <h2>Product List Coming Soon</h2>
+    </div>
+  );
+};
+
+export default ProductList;
